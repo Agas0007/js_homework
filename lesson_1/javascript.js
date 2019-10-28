5a1586a944fe82253264db0cff9f36d410ac5e23
Третьяков Олег <ichimaru859@gmail.com>
2019-10-29 00:43:10 +0500

    Mandatory hw. Lesson 1

---
+++ lesson_1/javascript.js
@@ -0,0 +1,10 @@
+var money;
+var income;
+var addExpenses;
+var deposit;
+var mission;
+var perion;
+
+alert("Hello! Please look at the console)");
+
+console.log("Use google, don't reinvent the wheel!");
