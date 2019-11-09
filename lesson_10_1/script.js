1c2bae79ac10482274f2abf1e85c369fe6a49c8e
Третьяков Олег <ichimaru859@gmail.com>
2019-11-10 00:13:01 +0500

    Edit modified:   lesson_10_1/script.js

--- lesson_10_1/script.js
+++ lesson_10_1/script.js
@@ -1,30 +1,33 @@
 'use strict';
 
-let DomElement = {
-    selector: '',
-    height: '',
-    width: '',
-    bg: '',
-    fontSize: '',
-    createElement: function(selector, height, width, bg, fontSize){
-        let newElement;
-        if(selector[0] == '.'){
-            newElement = document.createElement('div');
-            newElement.setAttribute('class', 'block');
-            newElement.style = `height: ${height};
-            width: ${width};
-            background: ${bg};
-            font-size: ${fontSize};`;
-            document.addEventListener('DOMContentLoaded', () => {
-                document.body.appendChild(newElement);
-            });
-        } else if(selector[0] == '#'){
-            newElement = document.createElement('p');
-            newElement.setAttribute('id', 'best');
-            document.body.appendChild(newElement);
-        }
-    },
-};
+class DomElement {
+    constructor(selector, height, width, bg, fontSize){
+        this.selector = selector;
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.fontSize = fontSize;
 
-let newDomElement = Object.create(DomElement);
-newDomElement.createElement('.block', '100px', '100px', 'gray', '14px');
+        this.createElement = function(){
+            let newElement;
+            if(selector[0] == '.'){
+                newElement = document.createElement('div');
+                newElement.setAttribute('class', 'block');
+                newElement.style = `height: ${this.height};
+                width: ${this.width};
+                background: ${this.bg};
+                font-size: ${this.fontSize};`;
+                document.addEventListener('DOMContentLoaded', () => {
+                    document.body.appendChild(newElement);
+                });
+            } else if(selector[0] == '#'){
+                newElement = document.createElement('p');
+                newElement.setAttribute('id', 'best');
+                document.body.appendChild(newElement);
+            }
+        };
+    }
+}
+
+const newElem = new DomElement('.block', '100px', '100px', 'gray', '14px');
+newElem.createElement();
