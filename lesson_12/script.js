7ff599d174e9fa038b35186422f114d2bebd9fdf
Третьяков Олег <ichimaru859@gmail.com>
2019-11-10 23:21:32 +0500

    Mandatory hw. Lesson 12, item 2

---
+++ lesson_12/script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const timesOfDay = document.querySelector('#times-of-day'),
+    today = document.querySelector('#today'),
+    currentTime = document.querySelector('#current-time'),
+    newYear = document.querySelector('#new-year'),
+    week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+    function getTimeRemaining() {
+        let date = new Date(),
+            dateStop = new Date(`December 31, 2019`).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            seconds = Math.floor(timeRemaining % 60),
+            minutes = Math.floor((timeRemaining / 60) % 60),
+            hours = Math.floor(timeRemaining / 60 / 60),
+            day = Math.floor(timeRemaining / 60 / 60 / 24),
+            timeToString = date.toLocaleTimeString('en');            
+
+        if (hours <= 9) {
+            hours = '0' + hours;
+        }
+        if (minutes <= 9) {
+            minutes = '0' + minutes;
+        }
+        if (seconds <= 9) {
+            seconds = '0' + seconds;
+        }
+
+        return {
+            timeRemaining,
+            hours,
+            minutes,
+            seconds,
+            timeToString,
+            day,
+            date,
+        };
+    }
+
+function output(){
+    let timer = getTimeRemaining();
+
+    if (timer.hours >= 0 && timer.hours <= 6) {
+        timesOfDay.textContent = `Доброй ночи!`;
+    } else if (timer.hours >= 6 && timer.hours < 12) {
+        timesOfDay.textContent = `Доброе утро!`;
+    } else if (timer.hours <= 18) {
+        timesOfDay.textContent = `Добрый день!`;
+    } else if (timer.hours >= 18) {
+        timesOfDay.textContent = `Добрый вечер!`;
+    }
+
+    today.textContent = 'Сегодня: ' + week[timer.date.getDay()];
+
+    function currentDateAndTime(){
+        let timer = getTimeRemaining();
+        currentTime.textContent = `Текущее время: ${timer.timeToString}`;
+        setInterval(currentDateAndTime, 1000);    
+    }
+    currentDateAndTime();
+
+    if(timer.timeRemaining > 0){
+        newYear.textContent = `До Нового года осталось ${timer.day} дней`;
+    } else{
+        newYear.textContent = '';
+    }    
+    
+}
+
+output();
